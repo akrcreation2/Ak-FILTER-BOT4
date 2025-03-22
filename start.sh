01e5dad8f164e3f22f98013d5f0b7b187fa2391c
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2025-03-22 22:35:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mn-bots/shobanafilterbot.git /shobanafilterbot
+  git clone https://github.com/akrcreation2/Ak-FILTER-BOT4.git /shobanafilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /shobanafilterbot
