4abe5c544cd043e37b917b990cc5490e6a16e51e
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-05-25 11:03:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -225,7 +225,7 @@ async def start(client, message):
     if not files_:
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
-             if message.from_user.id not in ADMINS:
+            if message.from_user.id not in ADMINS:
                 if not await check_verification(client, message.from_user.id) and VERIFY == True:
                     btn = [[
                         InlineKeyboardButton("ᴠᴇʀɪғʏ", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
