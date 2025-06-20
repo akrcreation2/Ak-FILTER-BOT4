daee01a9b741615dccfd5dbac2e50149287a3c23
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2025-06-20 21:52:30 +0530

    Update mntgxo.py

--- plugins/mntgxo.py
+++ plugins/mntgxo.py
@@ -1,6 +1,8 @@
+
 from pyrogram import filters, Client
 from pyrogram.types import Message
 from utils import JOIN_REQUEST_USERS
+from info import ADMINS
 
 @Client.on_message(filters.command("clear_join_users") & filters.user(ADMINS))
 async def clear_join_users(_, message: Message):
