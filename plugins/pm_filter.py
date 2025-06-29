b380e1c4b0e25dc8604b2f8b3c182663d4c0fa4d
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2025-06-29 13:58:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -694,23 +694,15 @@ async def auto_filter(client, msg, spoll=False):
                 for file in files
             ]
         if offset != "":
-        key = f"{message.chat.id}-{message.id}"
-        BUTTONS[key] = search
-        req = message.from_user.id if message.from_user else 0
-        btn.append([
-            InlineKeyboardButton(
-                text=f"📃 1/{math.ceil(int(total_results) / 10)}",
-                callback_data="pages"
-            ),
-            InlineKeyboardButton(
-                text="NEXT ▶️",
-                callback_data=f"next_{req}_{key}_{offset}"
-            )
-        ])
-    else:
-        btn.append([
-            InlineKeyboardButton(text="📃 1/1", callback_data="pages")
-        ])
+            key = f"{message.chat.id}-{message.id}"
+            BUTTONS[key] = search
+            req = message.from_user.id if message.from_user else 0
+            btn.append([
+                InlineKeyboardButton(text=f"📃 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
+                InlineKeyboardButton(text="NEXT ▶️", callback_data=f"next_{req}_{key}_{offset}")
+            ])
+        else:
+            btn.append([InlineKeyboardButton(text="📃 1/1", callback_data="pages")])
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
