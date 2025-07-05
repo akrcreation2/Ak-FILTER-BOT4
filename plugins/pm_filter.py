7d7d09a782834754c6eb664efa89edb889490f55
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2025-07-05 14:28:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -764,9 +764,9 @@ async def auto_filter(client, msg, spoll=False):
                 InlineKeyboardButton(text=f"📃 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
                 InlineKeyboardButton(text="NEXT ▶️", callback_data=f"next_{req}_{key}_{offset}")
             ])
-        else:
-            btn.append([InlineKeyboardButton(text="📃 1/1", callback_data="pages")])
-
+    
+    else:
+        btn.append([InlineKeyboardButton(text="📃 1/1", callback_data="pages")])
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
